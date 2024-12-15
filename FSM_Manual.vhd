088de305ec9397dc02425aed80268b318e45ece3
DanielvanReenen <81185248+DanielvanReenen@users.noreply.github.com>
2024-12-15 14:58:59 +0100

    Máquina de estados manual
    Esta es la máquina de estados que va a gestionar el modo manual. Lee los 4 inputs que le pueden entrar, hace los cambios oportunos dependiendo del estado en el que se encuentre, y retorna un vector de 12 bits con el valor de los 3 duty cycles (4 bits cada uno) y retorna otro vector de 2 bits con el color en el que se encuentra la máquina de estados en ese momento.

---
+++ FSM_Manual.vhd
@@ -0,0 +1,155 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11.12.2024 12:40:26
+-- Design Name: 
+-- Module Name: FSM_Manual - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+
+entity FSM_Manual is
+  port(
+    inputs     : in STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SETM       : in STD_LOGIC;
+    clk        : in STD_LOGIC;
+    DONE       : out STD_LOGIC := '0';
+    DUTYS      : out STD_LOGIC_VECTOR(11 DOWNTO 0) := "000000000000";
+    COLOR      : out STD_LOGIC_VECTOR(1 DOWNTO 0) := "11"
+    );
+end FSM_Manual;
+
+architecture Behavioral of FSM_Manual is
+-- Para la máquina de estados
+type STATE_M is (REPOSO, ROJO, VERDE, AZUL, SALIR);
+signal cur_state : STATE_M := REPOSO;
+signal nxt_state : STATE_M;
+
+-- Para fragmentar los duty cycles de cada color
+signal duty_original : std_logic_vector(11 downto 0);
+signal duty_rojo : unsigned(3 downto 0) := "0000";
+signal duty_verde: unsigned(3 downto 0) := "0000";
+signal duty_azul : unsigned(3 downto 0) := "0000";
+
+-- Registro de estados
+  begin
+    ST_REG : process(SETM, clk)   
+    begin
+    if SETM = '0' THEN
+      cur_state <= REPOSO;
+    elsif rising_edge(clk) then
+      cur_state <= nxt_state;
+    end if;
+    end process;
+    
+-- Avance de estados    
+    NEXT_STATE : process(inputs, cur_state)
+    begin
+      nxt_state <= cur_state;
+      case cur_state is
+        when REPOSO =>
+          if inputs = "0100" then
+            nxt_state <= ROJO;
+          elsif inputs = "1000" then
+            nxt_state <= SALIR;
+          end if;
+          
+        when ROJO =>
+          if inputs = "0100" then
+            nxt_state <= VERDE;
+          elsif inputs = "1000" then
+            nxt_state <= REPOSO;
+          end if;
+          
+        when VERDE =>
+          if inputs = "0100" then
+            nxt_state <= AZUL;
+          elsif inputs = "1000" then
+            nxt_state <= ROJO;
+          end if;
+        
+        when AZUL =>
+          if inputs = "0100" then
+            nxt_state <= SALIR;
+          elsif inputs = "1000" then
+            nxt_state <= VERDE;
+          end if;
+        
+        when SALIR =>
+          if inputs = "0100" then
+            nxt_state <= REPOSO;
+          elsif inputs = "1000" then
+            nxt_state <= AZUL;
+          end if;
+          
+        when others =>
+            report "ERROR: Invalid state transition in NEXT_STATE process.";
+            nxt_state <= REPOSO; 
+            
+      end case;  
+    end process;
+    
+-- Acción realizada en cada estado
+    STATE_ACTION : process(inputs, cur_state)
+    
+    begin
+      case cur_state is
+        when REPOSO =>
+            DONE <= '0';
+            COLOR <= "11";
+          
+        when ROJO =>
+            DONE <= '0';
+            if inputs = "0001" and duty_rojo /= "0000" then
+              duty_rojo <= duty_rojo - 1;
+            elsif inputs = "0010" and duty_rojo /= "1111" then
+              duty_rojo <= duty_rojo + 1;
+            end if;
+            duty_original(11 downto 8) <= STD_LOGIC_VECTOR(duty_rojo);
+            COLOR <= "10";
+              
+        when VERDE =>
+            DONE <= '0';
+            if inputs = "0001" and duty_verde /= "0000" then
+              duty_verde <= duty_verde - 1;
+            elsif inputs = "0010" and duty_verde /= "1111" then
+              duty_verde <= duty_verde + 1;
+            end if;
+            duty_original(7 downto 4) <= STD_LOGIC_VECTOR(duty_verde);
+            COLOR <= "01";
+          
+        when AZUL =>
+            DONE <= '0';
+            if inputs = "0001" and duty_azul /= "0000" then
+              duty_azul <= duty_azul - 1;
+            elsif inputs = "0010" and duty_azul /= "1111" then
+              duty_azul <= duty_azul + 1;
+            end if;
+            duty_original(3 downto 0) <= STD_LOGIC_VECTOR(duty_azul);
+            COLOR <= "00";
+
+        when SALIR =>
+            DUTYS <= duty_original;
+            DONE <= '1';
+            COLOR <= "11";
+      
+      end case; 
+    end process;
+          
+end Behavioral;
---
+++ FSM_Manual_tb.vhd
@@ -0,0 +1,201 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11.12.2024 23:36:43
+-- Design Name: 
+-- Module Name: FSM_Manual_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity FSM_Manual_tb is
+end FSM_Manual_tb;
+
+architecture Behavioral of FSM_Manual_tb is
+  -- Señales para conectar con la entidad a probar
+  signal inputs_tb : std_logic_vector(3 downto 0);
+  signal SETM_tb   : std_logic;
+  signal clk_tb    : std_logic := '0';
+  signal DONE_tb   : std_logic;
+  signal DUTYS_tb  : std_logic_vector(11 downto 0);
+  signal COLOR_tb  : std_logic_vector(1 downto 0);
+
+  -- Constante para el periodo del reloj
+  constant clk_period : time := 10 ns;
+
+begin
+
+  -- Instancia de la FSM a probar
+  DUT : entity work.FSM_Manual
+    port map (
+      inputs => inputs_tb,
+      SETM   => SETM_tb,
+      clk    => clk_tb,
+      DONE   => DONE_tb,
+      DUTYS  => DUTYS_tb,
+      COLOR  => COLOR_tb
+    );
+
+  -- Generador de reloj
+  clk_process : process
+  begin
+    for i in 0 to 100 loop -- Número limitado de ciclos
+      clk_tb <= '0';
+      wait for clk_period / 2;
+      clk_tb <= '1';
+      wait for clk_period / 2;
+    end loop;
+    wait; -- Termina el proceso
+  end process;
+
+  -- Proceso de estímulos
+  stimulus_process : process
+  begin
+    -- Inicialización
+    SETM_tb <= '0';
+    inputs_tb <= (others => '0');
+    wait for 20 ns;
+
+    -- Activar la máquina de estados
+    SETM_tb <= '1';
+
+    -- Cambiar al estado ROJO e incrementar su duty
+    inputs_tb <= "0100";
+    wait for clk_period;
+    inputs_tb <= "0010";
+    wait for clk_period;
+
+    -- Cambiar al estado VERDE e incrementar su duty
+    inputs_tb <= "0100"; 
+    wait for clk_period;
+    inputs_tb <= "0010";
+    wait for clk_period;
+
+    -- Cambiar al estado AZUL e incrementar su duty
+    inputs_tb <= "0100"; 
+    wait for clk_period;
+    inputs_tb <= "0010";
+    wait for clk_period;
+
+    -- Salir 
+    inputs_tb <= "0100";  
+    wait for clk_period;
+    
+    inputs_tb <= "0000";
+    wait for 3 * clk_period;
+    
+    -- Cambiar al estado AZUL y disminuir su duty
+    inputs_tb <= "1000";  
+    wait for clk_period;
+    inputs_tb <= "0001";
+    wait for 2 * clk_period;
+
+    -- Cambiar al estado VERDE y dismuinuir su duty
+    inputs_tb <= "1000";
+    wait for clk_period;  
+    inputs_tb <= "0001";
+    wait for clk_period;
+
+    -- Cambiar al estado ROJO y disminuir su duty
+    inputs_tb <= "1000";  
+    wait for clk_period;
+    inputs_tb <= "0001";
+    wait for clk_period;
+    
+    -- Salir 
+    inputs_tb <= "1000";  
+    wait for 2 * clk_period;
+    
+    inputs_tb <= "0000";
+    wait for 3 * clk_period;
+    
+-- Repetir el ciclo pero cambiando valores
+     
+    -- Cambiar al estado ROJO e incrementar su duty
+    inputs_tb <= "0100";
+    wait for 2 * clk_period;
+    inputs_tb <= "0010";
+    wait for clk_period;
+    inputs_tb <= "0000";
+    wait for clk_period;
+    inputs_tb <= "0010";
+    wait for clk_period;
+    
+
+    -- Cambiar al estado VERDE e incrementar su duty
+    inputs_tb <= "0100"; 
+    wait for clk_period;
+    inputs_tb <= "0010";
+
+    wait for clk_period;
+
+    -- Cambiar al estado AZUL e incrementar su duty
+    inputs_tb <= "0100"; 
+    wait for clk_period;
+    inputs_tb <= "0010";
+    wait for clk_period;
+    inputs_tb <= "0000";
+    wait for clk_period;
+    inputs_tb <= "0010";
+    wait for clk_period;
+    inputs_tb <= "0000";
+    wait for clk_period;
+    inputs_tb <= "0010";
+    wait for clk_period;
+
+    -- Salir 
+    inputs_tb <= "0100";  
+    wait for clk_period;
+    
+    inputs_tb <= "0000";
+    wait for 3 * clk_period;
+    
+    -- Cambiar al estado AZUL y disminuir su duty
+    inputs_tb <= "1000";  
+    wait for clk_period;
+    inputs_tb <= "0001";
+    wait for clk_period;
+
+    -- Cambiar al estado VERDE y dismuinuir su duty
+    inputs_tb <= "1000";
+    wait for clk_period;  
+    inputs_tb <= "0001";
+    wait for clk_period;
+
+    -- Cambiar al estado ROJO y disminuir su duty
+    inputs_tb <= "1000";  
+    wait for clk_period;
+    inputs_tb <= "0001";
+    wait for clk_period;
+    
+    -- Salir 
+    inputs_tb <= "1000";  
+    wait for 2 * clk_period;
+    
+    inputs_tb <= "0000";
+    wait for 3 * clk_period;
+
+
+    -- Finalizar la simulación
+    assert false 
+        report "Fin de la simulación." 
+        severity failure;
+ 
+  end process;
+
+end Behavioral;
