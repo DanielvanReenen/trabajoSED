dc4384c7d7fced200a7abce498bd0fafa2db31d2
manueldelgado <163348340+xmanu14hd@users.noreply.github.com>
2024-12-17 13:11:10 +0100

    Update FSM_Manual_tb.vhd

--- Manual/FSM_Manual_tb.vhd
+++ Manual/FSM_Manual_tb.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11.12.2024 23:36:43
--- Design Name: 
--- Module Name: FSM_Manual_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -27,7 +6,7 @@ entity FSM_Manual_tb is
 end FSM_Manual_tb;
 
 architecture Behavioral of FSM_Manual_tb is
-  -- Señales para conectar con la entidad a probar
+  -- SeÃ±ales para conectar con la entidad a probar
   signal inputs_tb : std_logic_vector(3 downto 0);
   signal SETM_tb   : std_logic;
   signal clk_tb    : std_logic := '0';
@@ -35,7 +14,7 @@ architecture Behavioral of FSM_Manual_tb is
   signal COLOR_tb  : std_logic_vector(1 downto 0);
 
   -- Constante para el periodo del reloj
-  constant clk_period : time := 10 ns;
+  constant clk_period : time := 1 ns;
 
 begin
 
@@ -52,7 +31,7 @@ begin
   -- Generador de reloj
   clk_process : process
   begin
-    for i in 0 to 100 loop -- Número limitado de ciclos
+    for i in 0 to 100 loop -- NÃºmero limitado de ciclos
       clk_tb <= '0';
       wait for clk_period / 2;
       clk_tb <= '1';
@@ -61,139 +40,58 @@ begin
     wait; -- Termina el proceso
   end process;
 
-  -- Proceso de estímulos
+  -- Proceso de estÃ­mulos
   stimulus_process : process
   begin
-    -- Inicialización
+    -- InicializaciÃ³n
     SETM_tb <= '0';
     inputs_tb <= (others => '0');
+    wait for clk_period ;
+
+    -- Activar la mÃ¡quina de estados
+    SETM_tb <= '1';
+    wait for clk_period ;
+    -- Cambiar al estado ROJO e incrementar su duty
     wait for 20 ns;
 
-    -- Activar la máquina de estados
-    SETM_tb <= '1';
-
-    -- Cambiar al estado ROJO e incrementar su duty
     inputs_tb <= "0100";
-    wait for clk_period;
-    inputs_tb <= "0010";
-    wait for clk_period;
-
-    -- Cambiar al estado VERDE e incrementar su duty
-    inputs_tb <= "0100"; 
-    wait for clk_period;
-    inputs_tb <= "0010";
-    wait for clk_period;
-
-    -- Cambiar al estado AZUL e incrementar su duty
-    inputs_tb <= "0100"; 
-    wait for clk_period;
-    inputs_tb <= "0010";
-    wait for clk_period;
-
-    -- Salir 
-    inputs_tb <= "0100";  
-    wait for clk_period;
-    
+    wait for 25 ns ;
     inputs_tb <= "0000";
-    wait for 3 * clk_period;
     
-    -- Cambiar al estado AZUL y disminuir su duty
-    inputs_tb <= "1000";  
-    wait for clk_period;
+    wait for 20 ns ;   
     inputs_tb <= "0010";
-    wait for 2 * clk_period;
-
-    -- Cambiar al estado VERDE y dismuinuir su duty
-    inputs_tb <= "1000";
-    wait for clk_period;  
-    inputs_tb <= "0010";
-    wait for clk_period;
-
-    -- Cambiar al estado ROJO y disminuir su duty
-    inputs_tb <= "1000";  
-    wait for clk_period;
-    inputs_tb <= "0010";
-    wait for clk_period;
-    
-    -- Salir 
-    inputs_tb <= "1000";  
-    wait for 2 * clk_period;
-    
+    wait for 20 ns ;
     inputs_tb <= "0000";
-    wait for 3 * clk_period;
     
--- Repetir el ciclo pero cambiando valores
-     
-    -- Cambiar al estado ROJO e incrementar su duty
+     wait for 20 ns;       
+    inputs_tb <= "0010";
+    wait for 20 ns;
+    inputs_tb <= "0000";
+    wait for 20 ns;       
+    
+    
+        wait for 20 ns;
+
     inputs_tb <= "0100";
-    wait for 2 * clk_period;
-    inputs_tb <= "0010";
-    wait for clk_period;
+    wait for 25 ns ;
     inputs_tb <= "0000";
-    wait for clk_period;
-    inputs_tb <= "0010";
-    wait for clk_period;
     
-
-    -- Cambiar al estado VERDE e incrementar su duty
-    inputs_tb <= "0100"; 
-    wait for clk_period;
+    wait for 20 ns ;   
     inputs_tb <= "0010";
-
-    wait for clk_period;
-
-    -- Cambiar al estado AZUL e incrementar su duty
-    inputs_tb <= "0100"; 
-    wait for clk_period;
-    inputs_tb <= "0010";
-    wait for clk_period;
+    wait for 20 ns ;
     inputs_tb <= "0000";
-    wait for clk_period;
+    
+     wait for 20 ns;       
     inputs_tb <= "0010";
-    wait for clk_period;
+    wait for 20 ns;
     inputs_tb <= "0000";
-    wait for clk_period;
-    inputs_tb <= "0010";
-    wait for clk_period;
+    wait for 20 ns;       
 
-    -- Salir 
-    inputs_tb <= "0100";  
-    wait for clk_period;
     
-    inputs_tb <= "0000";
-    wait for 3 * clk_period;
-    
-    -- Cambiar al estado AZUL y disminuir su duty
-    inputs_tb <= "1000";  
-    wait for clk_period;
-    inputs_tb <= "0001";
-    wait for clk_period;
-
-    -- Cambiar al estado VERDE y dismuinuir su duty
-    inputs_tb <= "1000";
-    wait for clk_period;  
-    inputs_tb <= "0001";
-    wait for clk_period;
-
-    -- Cambiar al estado ROJO y disminuir su duty
-    inputs_tb <= "1000";  
-    wait for clk_period;
-    inputs_tb <= "0001";
-    wait for clk_period;
-    
-    -- Salir 
-    inputs_tb <= "1000";  
-    wait for 2 * clk_period;
-    
-    inputs_tb <= "0000";
-    wait for 3 * clk_period;
+   
 
 
-    -- Finalizar la simulación
-    assert false 
-        report "Fin de la simulación." 
-        severity failure;
- 
+    wait;
   end process;
 
 end Behavioral;
